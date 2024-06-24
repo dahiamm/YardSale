760d0c0e3a99a74fe566666bf8eb9da029413297
Dahiana Muñoz <dahiammm@gmail.com>
2024-06-24 00:04:14 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -2,13 +2,42 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const burguerMenuIcon = document.querySelector('.menu')
+const cartIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 burguerMenuIcon.addEventListener('click', toggleMobileMenu)
+cartIcon.addEventListener('click', toggleAsideCart)
 
 function toggleDesktopMenu() {
+    const isMenuEmailClosed = aside.classList.contains('inactive')
+
+    if (!isMenuEmailClosed) {
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
+}
+function toggleAsideCart() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isMenuEmailClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    if (!isMenuEmailClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
